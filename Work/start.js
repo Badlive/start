700239ef63b3f822b8a83ae5449f76c40ed0ae8f
Badlive <ibadlive@gmail.com>
2025-02-02 23:17:45 +0300

    added README.md

---
+++ Work/start.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+var a = 2;
+var b = 3;
+console.log(a+b);
+
+axios.get('https://jsonplaceholder.typicode.com/users')
+  .then(res => {
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    console.log('Status Code:', res.status);
+    console.log('Date in Response header:', headerDate);
+
+    const users = res.data;
+
+    for(user of users) {
+      console.log(`Got user with id: ${user.id}, name: ${user.name}`);
+    }
+  })
+  .catch(err => {
+    console.log('Error: ', err.message);
+  });
